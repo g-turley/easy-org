254f0afe6fdfd3b5df360ff5e6ea1118fde21b01
Garrett Turley <gaturley@gmail.com>
2022-05-16 09:04:54 -0700

    BS 5 Update

--- app/library/helpers.py
+++ app/library/helpers.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import os
 import re
 import markdown
@@ -75,3 +76,82 @@ def org_protocol(request, content):
     # if test_get_response.status_code == 200:
     #     print(test_get_response.content.decode('utf-8'))
         # print(json.loads(test_get_response.content.decode('utf-8')))
+=======
+import os
+import re
+import markdown
+import requests
+from urllib import parse
+
+class OrgFile:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        with open(filepath, "r", encoding="utf-8") as input_file:
+            self.orglines = input_file.readlines()
+            # for line in orglines:
+            #     if line.startswith(":PROPERTIES:")
+
+
+def openfile(filename):
+    filepath = os.path.join("app/pages/", filename)
+    with open(filepath, "r", encoding="utf-8") as input_file:
+        text = input_file.read()
+
+    html = markdown.markdown(text)
+    data = {
+        "text": html
+    }
+    return data
+
+def listorg(current_dir="\\"):
+    if current_dir != "\\":
+        print(current_dir)
+        last_dir = current_dir
+        link_up = "\\".join(last_dir.split("\\")[:-1])
+        if link_up == "":
+            link_up = "\\"
+    else:
+        last_dir = "\\"
+        link_up = "\\"
+    dir_files = []
+    # Not sure if this is necessarily safe
+    dir_scan = f".\\app\\org\\{current_dir}"
+    dir_scan = dir_scan.replace("\\", os.sep)
+    with os.scandir(dir_scan) as it:
+        for entry in it:
+            if not entry.name.startswith("."):
+                if entry.is_dir():
+                    dir_files.append(entry.name)
+                elif entry.is_file():
+                    if entry.name.endswith('.org'):
+                        dir_files.append(entry.name)
+    data = {
+        "files": dir_files,
+        "dir": last_dir,
+        "link_up": link_up
+    }
+    return data
+
+def readorg(filename):
+    filepath = os.path.join(".\\app\\org\\", filename)
+    filepath = filepath.replace("\\", os.sep)
+    orgfile = OrgFile(filepath)
+    # Get properties from file
+    data = {
+        "lines": orgfile.orglines
+    }
+    return data
+
+def org_protocol(request, content):
+    title = "False title"
+    url = "localhost"
+    url = parse.quote(url)
+    title = parse.quote(title)
+    body = parse.quote(content)
+    print(body)
+    # Super unsafe, but we expect this.
+    return os.system(f'emacsclient "org-protocol://capture?template=w&url={url}&title={title}&body={body}"')
+    # if test_get_response.status_code == 200:
+    #     print(test_get_response.content.decode('utf-8'))
+        # print(json.loads(test_get_response.content.decode('utf-8')))
+>>>>>>> Stashed changes
--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
@@ -56,3 +57,64 @@ async def orgfile(request: Request, file: str, path: str):
 async def show_page(request: Request, page_name: str):
     data = openfile(page_name+".md")
     return templates.TemplateResponse("page.html", {"request": request, "data": data})
+=======
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from .library.helpers import *
+from app.routers import twoforms, unsplash, accordion
+
+app = FastAPI()
+
+
+templates = Jinja2Templates(directory="templates")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+app.include_router(unsplash.router)
+app.include_router(twoforms.router)
+app.include_router(accordion.router)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    data = openfile("home.md")
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
+
+@app.get("/files", response_class=HTMLResponse)
+async def orgpath(request: Request, path: str = "\\", response: str = ""):
+    data = listorg(path)
+    return templates.TemplateResponse("files.html", {"request": request, "data": data, "response": response})
+# Capture templates
+@app.get("/capture/new", response_class=HTMLResponse)
+async def orgfile(request: Request):
+    data = {}
+    return templates.TemplateResponse("capture.html", {"request": request, "data": data})
+
+# Org Editing
+@app.get("/org/new", response_class=HTMLResponse)
+async def orgfile(request: Request, content: str):
+    response = org_protocol(request, content)
+    response = RedirectResponse(url='/org/tasks.org?response={response}&path=\\')
+    # response = RedirectResponse(url=f'/files?response={response}')
+    return response
+    # return templates.TemplateResponse("files.html", {"request": request, "data": data})
+
+@app.get("/org", response_class=HTMLResponse)
+async def orgfile(request: Request, file: str, path: str):
+    data = listorg(os.path.join(path, file))
+    return templates.TemplateResponse("files.html", {"request": request, "data": data})
+
+@app.get("/org/{file}", response_class=HTMLResponse)
+async def orgfile(request: Request, file: str, path: str):
+    data = readorg(os.path.join(path, file))
+    return templates.TemplateResponse("org.html", {"request": request, "data": data})
+
+# Non-org viewing
+@app.get("/page/{page_name}", response_class=HTMLResponse)
+async def show_page(request: Request, page_name: str):
+    data = openfile(page_name+".md")
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
+>>>>>>> Stashed changes
